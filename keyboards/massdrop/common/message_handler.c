79fcb5a1730661f32acbccb4fac984e4cf28481f
dcerrone <dcerrone@tensentric.com>
2019-05-04 09:32:20 -0600

    Update hid protocol.

--- keyboards/massdrop/common/message_handler.c
+++ keyboards/massdrop/common/message_handler.c
@@ -17,20 +17,8 @@ void handle_raw_hid_command(uint8_t *data, uint8_t length)
       command_data[1] = HID_PROTOCOL_VER & 0xFF;
       break;
 
-    case message_id_jump_to_bootloader:
-      bootloader_jump();
-      break;
-
     case message_id_set_rgb_mode:
       rgblight_mode(command_data[0]);
-      break;
-
-    case message_id_set_rgb_hsv:
-      rgblight_sethsv(((uint16_t)((uint16_t)command_data[0] << (uint8_t)8) | (uint16_t)command_data[1]), command_data[2], command_data[3]);
-      break;
-
-    case message_id_set_rgb_hsv_noeeprom:
-      rgblight_sethsv_noeeprom(((uint16_t)((uint16_t)command_data[0] << (uint8_t)8) | (uint16_t)command_data[1]), command_data[2], command_data[3]);
       break;
 
     case message_id_increase_brightness:
@@ -39,32 +27,6 @@ void handle_raw_hid_command(uint8_t *data, uint8_t length)
 
     case message_id_decrease_brightness:
       rgblight_decrease_val();
-      break;
-
-    case message_id_get_mute_led_enabled:
-      break;
-
-    case message_id_set_mute_led_enabled:
-      break;
-
-#ifdef DYNAMIC_KEYMAP_ENABLE
-    case message_id_get_dynamic_keymap: ;
-      uint16_t keycode = dynamic_keymap_get_keycode( command_data[0], command_data[1], command_data[2] );
-			command_data[3] = keycode >> 8;
-			command_data[4] = keycode & 0xFF;
-      break;
-
-    case message_id_set_dynamic_keymap:
-      dynamic_keymap_set_keycode( command_data[0], command_data[1], command_data[2], ( command_data[3] << 8 ) | command_data[4] );
-      break;
-#endif
-
-    case message_id_set_macro: ;
-
-      break;
-
-    case message_id_get_macro: ;
-
       break;
 
     default:
--- keyboards/massdrop/common/message_id.h
+++ keyboards/massdrop/common/message_id.h
@@ -4,24 +4,15 @@
 #define MESSAGE_ID_H
 
 //Device protocol version
-#define HID_PROTOCOL_VER 2
+#define HID_PROTOCOL_VER 1
 
 //HID protocol version 1
 enum message_id {
   message_id_protocol_version = 0x01,
-  message_id_jump_to_bootloader,
 
   message_id_set_rgb_mode,
-  message_id_set_rgb_hsv,
-  message_id_set_rgb_hsv_noeeprom,
   message_id_increase_brightness,
   message_id_decrease_brightness,
-  message_id_get_dynamic_keymap,
-  message_id_set_dynamic_keymap,
-  message_id_get_mute_led_enabled,
-  message_id_set_mute_led_enabled,
-  message_id_set_macro,
-  message_id_get_macro,
 };
 
 #endif //RAW_ENABLE
